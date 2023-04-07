6f9c2901df7f5934b23f4cf6fffc823262be8d09
MPChege <mark.wanyoike@strathmore.edu>
2023-04-07 23:10:58 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+// Function expression for dividing two numbers
+const divide = function(num1, num2) {
+    return num1 / num2;
+  }
+  
+  // Arrow function for squaring a number
+  const square = (num) => {
+    return num * num;
+  }
+  
+  // Arrow function for adding two numbers
+  const add = (num1, num2) => {
+    return num1 + num2;
+  }
+  
+  module.exports = { divide, square, add };
+  
